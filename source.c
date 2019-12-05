0978936d1005ad2e97c5c0d4094ff5f0462eb96a
Nikola <malikovic.nikola@hotmail.com>
2019-12-05 04:12:35 +0100

    -Dodao platformu gde se nalaze igraci i neprijatelji u obliku vise kocki. -Dodao loptu kao privremenog igraca -Dodao pomeranje igraca po terenu -Promenio boju platforme i igraca

--- source.c
+++ source.c
@@ -7,6 +7,8 @@ static void on_reshape(int width, int height);
 static void on_display(void);
 
 const static float pi = 3.141592653589793;
+static float ball_x_movement=0;
+static float ball_y_movement=0;
 
 static float phi , theta ;
 static float delta_phi , delta_theta ;
@@ -14,14 +16,10 @@ static float delta_phi , delta_theta ;
 int main(int argc,char** argv)
 {
     
-    
     GLfloat light_ambient[] = { 1, 1, 1, 1 };
     GLfloat light_diffuse[] = { 1, 1, 1, 1 };
     GLfloat light_specular[] = { 1, 1, 1, 1 };
-    GLfloat ambient_coeffs[] = { 0.647059, 0.164706, 0.164706, 1 };
-    GLfloat diffuse_coeffs[] = { 0, 0, 0, 1 };
-    GLfloat specular_coeffs[] = { 1, 1, 1, 1 };
-    GLfloat shininess = 30;
+    
     
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
@@ -41,10 +39,7 @@ int main(int argc,char** argv)
     glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
     glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
 
-    glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_coeffs);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse_coeffs);
-    glMaterialfv(GL_FRONT, GL_SPECULAR, specular_coeffs);
-    glMaterialf(GL_FRONT, GL_SHININESS, shininess);
+
     
     
     phi = pi/4;
@@ -68,8 +63,26 @@ static void on_keyboard(unsigned char key, int x, int y)
     case 27:
         exit(0);
         break;
-        
+    case 'w':
+        ball_x_movement+=-0.2;
+        glutPostRedisplay();
+        break;
+    case 's':
+        ball_x_movement+=0.2;
+        glutPostRedisplay();
+        break;
     case 'a':
+        ball_y_movement+=0.2;
+        glutPostRedisplay();
+        break;
+    case 'd':
+        ball_y_movement+=-0.2;
+        glutPostRedisplay();
+        break;
+        
+        
+        
+    case 'j':
         phi -= delta_phi;
         if (phi > 2 * pi) {
             phi -= 2 * pi;
@@ -78,14 +91,14 @@ static void on_keyboard(unsigned char key, int x, int y)
         }
         glutPostRedisplay();
         break;
-    case 'w':
+    case 'i':
         theta += delta_theta;
         if (theta > pi / 2 - pi / 180) {
             theta = pi / 2 - pi / 180;
         }
         glutPostRedisplay();
         break;
-    case 'd':
+    case 'l':
         phi += delta_phi;
         if (phi > 2 * pi) {
             phi -= 2 * pi;
@@ -94,7 +107,7 @@ static void on_keyboard(unsigned char key, int x, int y)
         }
         glutPostRedisplay();
         break;
-    case 's':
+    case 'k':
         theta -= delta_theta;
         if (theta < -(pi / 2 - pi / 180)) {
             theta = -(pi / 2 - pi / 180);
@@ -106,11 +119,11 @@ static void on_keyboard(unsigned char key, int x, int y)
 
 static void on_reshape(int width, int height)
 {
-     glViewport(0, 0, width, height);
+    glViewport(0, 0, width, height);
     
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(60, (float) width / height, 1, 10);
+    gluPerspective(90, (float) width / height, 1, 20);
 }
 
 static void on_display(void)
@@ -143,12 +156,69 @@ static void on_display(void)
         glVertex3f(-0.5+i, 0.5, 0);
         glVertex3f(0.5+i, 0.5, 0);
     }*/
-        glVertex3f(0.5, -0.5, 0);
+     /*   glVertex3f(0.5, -0.5, 0);
             glVertex3f(-0.5, -0.5, 0);
             glVertex3f(-0.5, 0.5, 0);
-            glVertex3f(0.5, 0.5, 0);
-    glEnd();
+            glVertex3f(0.5, 0.5, 0);*/
 
+	/*Kocke koje obelezavaju platforme na kojima ce se nalaziti igrac i protivnici*/
+	
+    glEnd();
+    /*rename Colors*/
+    
+    GLfloat ambient_coeffsBall[] = {0.1, 0.1, 0.4 ,1};
+    
+    GLfloat ambient_coeffs1[] = { 0.5, 0.164706, 0.164706, 1 };
+    GLfloat diffuse_coeffs1[] = { 0, 0, 0, 1 };
+    GLfloat specular_coeffs1[] = { 1, 1, 1, 1 };
+    GLfloat shininess1 = 10;
+    
+    GLfloat ambient_coeffsBlack[] = { 0, 0, 0, 1 };
+    GLfloat diffuse_coeffsBlack[] = { 0, 0, 0, 1 };
+    GLfloat specular_coeffs2[] = { 1, 1, 1, 1 };
+    GLfloat shininess2 = 10;
+    
+    int i,j;
+    glPushMatrix();
+        glTranslatef(0,0,-0.1);
+        for(i=0;i<10;i++)
+        {
+            glPushMatrix();
+            for(j=0;j<10;j++)
+            {
+                /*toRename*/
+                glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_coeffsBlack);
+                glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse_coeffsBlack);
+                glMaterialfv(GL_FRONT, GL_SPECULAR, specular_coeffs2);
+                glMaterialf(GL_FRONT, GL_SHININESS, shininess2); 
+                glutWireCube(0.2);
+                glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_coeffs1);
+                glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse_coeffs1);
+                glMaterialfv(GL_FRONT, GL_SPECULAR, specular_coeffs1);
+                glMaterialf(GL_FRONT, GL_SHININESS, shininess1); 
+                glutSolidCube(0.20);
+                glTranslatef(-0.2,0,0);
+            }
+            glPopMatrix();
+            glTranslatef(0,-0.2,0);
+        }
+    glPopMatrix();
+    
+    
+    glPushMatrix();
+        glTranslatef(ball_y_movement,ball_x_movement,0.1);
+        glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_coeffsBlack);
+        glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse_coeffsBlack);
+        glMaterialfv(GL_FRONT, GL_SPECULAR, specular_coeffs2);
+        glMaterialf(GL_FRONT, GL_SHININESS, shininess2); 
+            glutWireSphere(0.1,10,10);
+        glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_coeffsBall);
+        glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse_coeffsBlack);
+        glMaterialfv(GL_FRONT, GL_SPECULAR, specular_coeffs2);
+        glMaterialf(GL_FRONT, GL_SHININESS, shininess2); 
+            glutSolidSphere(0.1,10,10);
+    glPopMatrix();
+    
     glPushMatrix();
         glBegin(GL_LINES);
         glVertex3f(0,0,0);
